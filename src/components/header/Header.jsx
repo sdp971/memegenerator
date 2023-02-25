f23a0d5f434c8821e5cedc8545ca91cd14b759fb
sdp971 <doris190485@hotmail.com>
2023-02-25 13:55:58 +0100

    added header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.css";
+
+const Header = (props) => {
+  return (
+    <header>
+      <h2 className='header--title'>Meme Generator</h2>
+      <p className='header--project'>React Personal Project</p>
+      
+    </header>
+  )
+}
+
+export default Header
