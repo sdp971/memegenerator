7fb1d86db3dace20b1935dd8df5adcadb4ab94bc
sdp971 <doris190485@hotmail.com>
2023-07-06 22:25:36 +0200

    created form and adding text on img (functionality)

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,21 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Form.css';
+import memesData from '../../memesData';
 
 const Form = (props) => {
-  return (
-    <form className='form-wrapper'>
-      <div className='input-wrapper'>
-        <input
-          type='htmlText'
-          placeholder='Top text'/>
-        
-        <input
-          type='htmlText'
-          placeholder='Bottom text'/>
-      </div>
+  const [meme, setMeme] = useState({
+    topText: '',
+    bottomText: '',
+    randomImage: 'http://i.imgflip.com/1bij.jpg',
+  });
 
-      <button type='submit'>Get a new meme image 🖼️ </button>
-    </form>
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
+
+  const getMemeImage = () => {
+    const memesArray = allMemeImages.data.memes;
+    console.log(memesArray);
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    console.log(url);
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+     
+    }));
+  };
+
+  function handleClick() {
+    getMemeImage();
+  }
+
+  useEffect(() => {
+    getMemeImage();
+  }, [allMemeImages]);
+
+  function handleChange(event) {
+     const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+  
+
+  return (
+    <div>
+      <form className='form-wrapper'>
+        <div className='input-wrapper'>
+          <input
+            type='htmlText'
+            placeholder='Texte du haut'
+            aria-label='string'
+            name='topText'
+            value={meme.topText}
+            onChange={handleChange}
+          />
+
+          <input
+            type='htmlText'
+            placeholder='Texte du bas'
+            aria-label='string'
+            name='bottomText'
+            value={meme.bottomText}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button type='submit' onClick={handleClick}>
+          Obtenir une nouvelle image 🖼️
+        </button>
+      </form>
+      <div className='meme'>
+        <img src={meme.randomImage} alt="" className='meme--image' />
+
+        <h2 className='meme--text top'>{meme.topText}</h2>
+        <h2 className='meme--text bottom'>{meme.bottomText}</h2>
+      </div>
+    </div>
   );
 };
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,10 +4,8 @@ import "./Header.css";
 const Header = (props) => {
   return (
     <header>
-      <h2 className='header--title'>Meme Generator</h2>
-      <p className='header--project'>React Personal Project</p>
-      
-    </header>
+      <h2 className='header--title'>Générateur de memes</h2>
+        </header>
   )
 }
 
